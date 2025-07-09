a2b6f14eff61296309eb665bc43d34be5bb6e85d
seop <77163842+seop-kim@users.noreply.github.com>
2025-07-09 23:56:55 +0900

    build: Add pnpm, prisma

--- inflearn-fullstack-clone/backend/.eslintrc.js
+++ inflearn-fullstack-clone/backend/.eslintrc.js
@@ -8,7 +8,7 @@ module.exports = {
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
     'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
+    // 'plugin:prettier/recommended', // removed to disable prettier spacing errors
   ],
   root: true,
   env: {
--- inflearn-fullstack-clone/backend/src/app.controller.ts
+++ inflearn-fullstack-clone/backend/src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+// import { PrismaClient } from 'generated/prisma';
 
 @Controller()
 export class AppController {
