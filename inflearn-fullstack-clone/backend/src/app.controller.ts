fddcfcf9f5058597b0dc8e02d67fe03c08425688
seop <77163842+seop-kim@users.noreply.github.com>
2025-07-14 23:14:16 +0900

    chore: Update .gitignore and remove unused import in app.controller
    - `.gitignore` 파일에서 `/generated/prisma` 대신 `/generated/`로 변경하였습니다.
    - `app.controller.ts` 파일에서 사용하지 않는 `PrismaClient` import를 제거하였습니다.

--- inflearn-fullstack-clone/backend/src/app.controller.ts
+++ inflearn-fullstack-clone/backend/src/app.controller.ts
@@ -1,6 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-// import { PrismaClient } from 'generated/prisma';
 
 @Controller()
 export class AppController {
